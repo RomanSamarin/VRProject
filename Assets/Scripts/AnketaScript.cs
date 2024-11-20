0b48c79e9cca9a0d0aa7380a5418d167d81da1d6
VR-KVANT-10\VR_10 <roman.samarin20120609@gmail.com>
2024-11-20 18:07:35 +0300

    add AnketaScripts

--- Assets/Scripts/AnketaScript.cs
+++ Assets/Scripts/AnketaScript.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class AnketaScript : MonoBehaviour
 {
-    public TMP_InputField nameInputField;
-    public TMP_Text nameTMPText;
-     public TMP_InputField ageInputField;
-     public TMP_Text ageTMPText;
-    public void OnButtonUserName() {
-     nameTMPText.text =nameInputField.text;
-    }
-    public void OnButtonAge() {
-       string ageString = ageInputField.text;
-        int ageInt = int.Parse(ageString);
-     }
+   public TMP_InputField nameInputField;
+   public TMP_Text nameTMPText;
+   public TMP_InputField ageInputField;
+   public TMP_Text birthDayYearTmpText;
+   public void OnButtonUserName() {
+      nameTMPText.text =nameInputField.text;
+   }
+   public void OnButtonUserAge() {
+      string ageString = ageInputField.text;
+      if (ageString != null && ageString.Length > 0) {
+         if (int.TryParse(ageString, out int ageInt)) {
+            DateTime dateTime = DateTime.Now;
+            dateTime = dateTime.AddYears(-ageInt);
+            birthDayYearTmpText.text =dateTime.ToString("yyyy");
+         } else {
+            birthDayYearTmpText.text = "Нужно только число!";
+         }
+      } else {
+         birthDayYearTmpText.text= "Не должно быть пустой!";
+      }
+   }
 }
